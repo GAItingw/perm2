90edffb24e891b82a8bc5f069ec533925c505aed
tingw <tingW@dhcp-wifi-8021x-155-41-106-205.bu.edu>
2016-11-30 15:38:51 -0500

    ting wang

--- example.R
+++ example.R
@@ -37,4 +37,42 @@ abline(v = test.diff, col = "black", lwd = 4)
 # 1 tail
 (sum(examples<test.diff))/(10000)
 
+#median
+#set the data
+set.seed(3) 
+grp.1 <- round(rlnorm(100, 6)) # With Widget
+grp.2 <- round(rlnorm(100, 6)) # Original page
 
+hist(grp.1)
+hist(grp.2)
+
+data <- c(grp.1, grp.2)
+
+l1 <- length(grp.1)
+l2 <- length(grp.2)
+lt <- l1+l2
+
+
+# null hypothesis is median old  = median new
+test.diff <- median(grp.1) - median(grp.2)
+
+it_median <- function(n){
+  M = NULL
+  for(i in 1:n){
+    s = sample(data, lt, FALSE)
+    m1 = median(s[1:l1]) - median(s[(l1+1):lt])
+    M = c(M,m1)
+  }
+  return(M)
+}
+
+examples <- it(10000)
+
+par(mfrow=c(1,1))
+hist(examples, col = "red", breaks = 100, main="Random Permutations", xlab="")
+abline(v = test.diff, col = "black", lwd = 4)
+abline(v = quantile(examples, 0.025), col = "blue", lwd = 4)
+abline(v = quantile(examples, 0.975), col = "orange", lwd = 4)
+
+length(examples[examples<test.diff])/10000
+# p-value = 0.4165 is too large
